75a480986c07ec36484dab05f11475272a72ddd5
boahs <39522778+boahs@users.noreply.github.com>
2019-10-14 15:33:21 -0400

    Create Quoting_Strings_with_Single_Quotes.js

---
+++ Solutions/Quoting_Strings_with_Single_Quotes.js
@@ -0,0 +1,7 @@
+var myStr = '<a href="http://www.boahs.info" target="_blank">Based Example</a>';
+
+//Remember, a string has the same kind of quote at the beginning and end. But if you have that same quote somewhere in the middle, the string will stop early and throw an error. Also the backslash \should not be be confused with the forward slash /. They do not do the same thing! 
+
+
+
+
