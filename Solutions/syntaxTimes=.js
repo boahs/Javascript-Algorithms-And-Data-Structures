1dd31dbf0a8b3b73c5dff7e24b9ecfee3e99b862
boahs <39522778+boahs@users.noreply.github.com>
2019-10-14 12:44:11 -0400

    Create syntaxTimes=.js

---
+++ Solutions/syntaxTimes=.js
@@ -0,0 +1,27 @@
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10; //*= is doing multiplication 
+
+
+//Example  
+
+var a = 5;
+
+a = a * 5; //this would = 25 ... but you can shorten using the *= 
+
+
+
+//~~~~~~~~~
+
+//var a = 5;
+
+a *= 5; //This also will equal 25. Shorter. Cleaner. Better.
+
+
+
+
+
