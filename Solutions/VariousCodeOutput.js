237c3fa2f7172a30fb73e22a3c9b94c4d8357546
boahs <39522778+boahs@users.noreply.github.com>
2019-10-14 16:56:51 -0400

    Create VariousCodeOutput.js

---
+++ Solutions/VariousCodeOutput.js
@@ -0,0 +1,8 @@
+//  \' singlequote
+//  \" double quote
+//  \\ backslash
+//  \n newline
+//  \t tab
+//  \b backspace
+//  \f formfeed
+// Note that the backslash itself must be escaped in order to display as a backslash.
