15ca96ebe33f22cb77954ff69f429632f48c7d0f
boahs <39522778+boahs@users.noreply.github.com>
2019-10-14 05:57:43 -0400

    Update assignments.js

--- Solutions/assignments.js
+++ Solutions/assignments.js
@@ -3,7 +3,7 @@ var a;
 var b = 2;
 
 // Only change code below this line
-var a;
+var a;     // I'm only adding this because I feel a new user would need to understand they are actually implicting the var - you don't need to write it again because it's already declared @ the top
 var b = 2;
 
 a = 7;
