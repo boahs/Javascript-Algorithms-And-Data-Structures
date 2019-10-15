79fdc1fcd211001295a085cda226e83d74dca84f
boahs <39522778+boahs@users.noreply.github.com>
2019-10-15 09:20:52 -0400

    Create Use_Bracket_Notation_To_Find_the_First_Character_in_a_String.js

---
+++ Solutions/Use_Bracket_Notation_To_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,4 @@
+var firstLetterOfFirstName = "";
+var lastName = "LoveWillSmith";
+
+firstLetterOfLastName = lastName[0]; //"Bracket Notation" - This is finding the first character in the lastName var, and assigning it to firstLetterOfLastName
