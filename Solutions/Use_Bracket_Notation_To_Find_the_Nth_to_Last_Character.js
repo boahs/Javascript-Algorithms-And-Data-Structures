398e405e86cdc9db2e1480a13da68cc8f0bddf97
boahs <39522778+boahs@users.noreply.github.com>
2019-10-15 09:35:45 -0400

    Create Use_Bracket_Notation_To_Find_the_Nth_to_Last_Character.js

---
+++ Solutions/Use_Bracket_Notation_To_Find_the_Nth_to_Last_Character.js
@@ -0,0 +1,3 @@
+var firstName = "Boahs";
+var thirdToTheLastLetterOfFirstName = firstName[firstName.length - 3]
+// Using bracket notation you can retrieve the last character in a string to retreive the Nth-to-last character.
