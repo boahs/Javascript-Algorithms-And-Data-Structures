e064ef403fed8e547e7d886facb31146456edd62
boahs <39522778+boahs@users.noreply.github.com>
2019-10-15 08:35:05 -0400

    Create concatenating_strings.js

---
+++ Solutions/concatenating_strings.js
@@ -0,0 +1,3 @@
+var mrLord = "This is an example of having a concatenated string";
+myLord += "onto an existing string variable."; // Watch out for spaces. Concatenation does not add spaces between concatenated strings, so you'll need to add them yourself.
+
