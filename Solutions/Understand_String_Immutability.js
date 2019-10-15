3ba31000336d89987c1294088fca42ebe9532bbd
boahs <39522778+boahs@users.noreply.github.com>
2019-10-15 09:24:01 -0400

    Create Understand_String_Immutability.js

---
+++ Solutions/Understand_String_Immutability.js
@@ -0,0 +1,4 @@
+var myStr = "This is IMMUTABILITY!";
+// Above is a string. These values are immutable, which means they cannot be altered once created. 
+
+myStr = "Hello World :)"; // But JS is magical so you can just declare it again, and literal magic. 
