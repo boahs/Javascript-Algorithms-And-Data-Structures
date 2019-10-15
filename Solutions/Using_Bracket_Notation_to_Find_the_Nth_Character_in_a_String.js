b5520a44fa16e8927844e563dd03c53eae0b170b
boahs <39522778+boahs@users.noreply.github.com>
2019-10-15 09:28:30 -0400

    Create Using_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js

---
+++ Solutions/Using_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,5 @@
+// Remember when a computer counts it starts at 0 - so the first character is always 0
+var firstName = "Boahs";
+var secondLetterOfFirstName = firstName[1]; 
+// You can find the second letter in a name using Bracket Notation just like finding the first character. It's that simple.
+// Output would be "o" not "B" since the computer starts counting at 0. 
