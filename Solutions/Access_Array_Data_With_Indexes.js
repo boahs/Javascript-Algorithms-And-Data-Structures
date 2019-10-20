41feec4b522396a1f194f9c5ab3235de19126512
boahs <39522778+boahs@users.noreply.github.com>
2019-10-20 11:25:03 -0400

    Create Access_Array_Data_With_Indexes.js

---
+++ Solutions/Access_Array_Data_With_Indexes.js
@@ -0,0 +1,3 @@
+var numbersArray = [420, 69, 42];
+var myData = numbersArray[0]; // equals 420
+//Accessing adata inside arrays using indexes. Works the same like bracket notation 0 = first, 1 = second. Etc. Zero-based indexing. 
