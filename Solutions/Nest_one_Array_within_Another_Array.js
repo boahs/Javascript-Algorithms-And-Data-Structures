2caa4f2c79882907534ec6f11d3caeb291b2284a
boahs <39522778+boahs@users.noreply.github.com>
2019-10-20 11:21:25 -0400

    Create Nest_one_Array_within_Another_Array.js

---
+++ Solutions/Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,2 @@
+var multidimensionalArray = [["the multiverse", 42], ["the internet", 101010]];
+// You can also nest arrays within other arrays. Funfact: These is also called a multi-dimensional Array.
