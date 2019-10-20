6e6f0074822baf4cebd64a2d347724fa95874bd5
boahs <39522778+boahs@users.noreply.github.com>
2019-10-20 11:06:33 -0400

    Create wordBlanks.js

---
+++ Solutions/wordBlanks.js
@@ -0,0 +1,6 @@
+var myNoun = "fat man";
+var myAdjective = "tall";
+var myVerb = "fisted";
+var myAdverb = "very fast";
+
+var wordBlanks = "The " + myAdjective + " " + myNoun + "" + myVerb + " " +myAdverb + "."; //This is an example of a word blank. To create a worblank for maybe a madlibs type game you would concatentate your variables above to form a whole string.
