0bc2b5879ca81b0f35ba787ae02963a97606f48c
boahs <39522778+boahs@users.noreply.github.com>
2019-10-23 12:52:35 -0400

    Create Write_Reusable_JavaScript_with_Functions.js

---
+++ Solutions/Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,4 @@
+function helloWorld() { // In JS you can divide your code up in reusable parts called functions
+	console.log("Hello world!");
+}
+helloWorld(); //This is called calling a function. It will print out what is inbetween the curly brackets
