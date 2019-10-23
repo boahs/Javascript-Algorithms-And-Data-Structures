6beca943fd7bdb59ef511f126b3020782ea12f54
boahs <39522778+boahs@users.noreply.github.com>
2019-10-23 13:23:10 -0400

    Create Passing_Values_To_Functions_with_Arguments.js

---
+++ Solutions/Passing_Values_To_Functions_with_Arguments.js
@@ -0,0 +1,11 @@
+function ourFunctionWithArgs(f, e) {
+	console.log(f + e);
+
+}
+ourFunctionWithArgs(4, 9);
+
+function ourFunctionWithArgsWords(test, placeholder, balls) {
+	
+	console.log(test, placeholder, balls)
+}
+ourFunctionWithArgsWords("Hello", "Boahs", "Dog") //These parameters are variables that will act like a placeholder for the values that are to be input. Can be done with any type of var.
