7800a6df0abea58abce86c714f0556c06cda8d3a
boahs <39522778+boahs@users.noreply.github.com>
2019-10-23 16:10:29 -0400

    Create Local_Scope_and_Functions.js

---
+++ Solutions/Local_Scope_and_Functions.js
@@ -0,0 +1,11 @@
+function myLocalScope() {
+	'use strict'; 
+	var myVar = "Hello Bud!"; //variables which are declared within a function have local scope. Only visible within this function
+	console.log(myVar);
+	}
+    myLocalScope();
+
+    var myVar
+    console.log(myVar);
+
+
