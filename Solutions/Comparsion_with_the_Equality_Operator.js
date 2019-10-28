8a93b266ad813c381d41761cec443f3f124de875
boahs <39522778+boahs@users.noreply.github.com>
2019-10-27 22:16:08 -0400

    Create Comparsion_with_the_Equality_Operator.js

---
+++ Solutions/Comparsion_with_the_Equality_Operator.js
@@ -0,0 +1,9 @@
+function testTruth(truth) {
+	if (truth == 420) { 
+	return "Right on brother;"
+	}
+	return "Oh well. Maybe next time.";
+}
+testTruth(16);
+testTruth(420);
+testTruth(41); // == can compare TWO values and returns true if they're equvalanet. Such as a string, and number. 
