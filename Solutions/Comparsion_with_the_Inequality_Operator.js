cb78c1fbe3607b1660dcce03481a74d9e801291f
boahs <39522778+boahs@users.noreply.github.com>
2019-10-30 19:39:13 -0400

    Create Comparsion_with_the_Inequality_Operator.js

---
+++ Solutions/Comparsion_with_the_Inequality_Operator.js
@@ -0,0 +1,8 @@
+function inequalityOperator(number) {
+	if (number != 420) {
+	return "Not 420 enough m8";
+
+	}
+	return "Ok good to go."
+}
+inequalityOperator(420); // != will always result in false
