b3ec0844e78bc8418cc101a07cffc0bae1fe57d1
boahs <39522778+boahs@users.noreply.github.com>
2019-11-02 18:19:44 -0400

    Create Comparsion_with_the_Greater_Than_Or_Equal_To_Operator.js

---
+++ Solutions/Comparsion_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,12 @@
+function greaterOrEqual(boahs) {
+	if (boahs>=28){
+	return "28 or over";
+	}
+	if (boahs>=16){
+	return "16 or over";
+	}
+	return "Less than 16";
+}
+greaterOrEqual(12); >= will be greater than, or equal. Number to the left is greater than or equal to the number on the right, it'll return true, otherwise it'll return false. 
+
+// Example : 3 >= '1' true || 1 >= 9 = false
