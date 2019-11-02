b0c491466acd6d8bcde2225d787fc654fc3ad0ed
boahs <39522778+boahs@users.noreply.github.com>
2019-11-02 18:15:10 -0400

    Create Comparsion_with_the_Greater_Than_Operator.js

---
+++ Solutions/Comparsion_with_the_Greater_Than_Operator.js
@@ -0,0 +1,12 @@
+function greaterOperator(great) {
+	if (great>420) {
+	return "Over 420.";
+	}
+
+    if (great>42){
+    return "Over 42.";
+    }
+
+    return "Under 10";
+}
+greaterOperator(420); //> Greater than value sign will return true if greater than the number to the right, or else false. 
