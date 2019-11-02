2ccfb49b430a572ff0e1596c7c58f0663792ee02
boahs <39522778+boahs@users.noreply.github.com>
2019-11-02 18:25:48 -0400

    Create Comparsion_with_the_Less_Than_Or_Equal_To_Operator.js

---
+++ Solutions/Comparsion_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,11 @@
+function lessOrEqual(number) {
+	if (number<=15) {
+	return "Smaller than or equal to 15";
+    }
+    if (number<=99) {
+    return "Smaller or equal to 99";
+}
+return "More than 99";
+}
+
+lessOrEqual(500);
