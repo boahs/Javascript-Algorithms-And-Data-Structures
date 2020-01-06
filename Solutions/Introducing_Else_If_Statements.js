165c897c3f6a65df111e9f8cedf96ae08bf0b616
boahs <39522778+boahs@users.noreply.github.com>
2020-01-06 10:05:42 -0500

    Create Introducing_Else_If_Statements.js

---
+++ Solutions/Introducing_Else_If_Statements.js
@@ -0,0 +1,17 @@
+function userAgeVeri(userAge){
+	
+    if (userAge > 25) { 
+	return "Welcome to the 25 year old site!";
+	
+	} 
+
+	  else if (userAge < 25) {
+		return "Not welcome.";
+	}
+	else {
+		return "FUCK OFF!";
+	}
+	userAgeVeri(28);
+	}
+
+//above is an example of utilizing the else if statement
