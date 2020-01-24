03adc39246e7fe347db04085f00909ad6082f491
boahs <39522778+boahs@users.noreply.github.com>
2020-01-24 10:07:29 -0500

    ~3 new statements

---
+++ Solutions/Multiple_Identical_Options_in_Switch_Statements.js
@@ -0,0 +1,24 @@
+function sequentialSizes(val) {
+  let answer = "";
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+    return "Low";
+    break;
+    case 4:
+    case 5:
+    case 6:
+    return "Mid";
+    break;
+    case 7:
+    case 8:
+    case 9:
+    return "High";
+    break;
+  }
+
+  return answer;
+}
+
+sequentialSizes(1);
