77fe5edceeaf5540df9d429c4a78987a6bb31188
boahs <39522778+boahs@users.noreply.github.com>
2020-04-15 13:23:15 -0400

    Create Returning Boolean Values from Functions.js

---
+++ Solutions/Returning Boolean Values from Functions.js
@@ -0,0 +1,15 @@
+function isLess(a,b) {
+    return a < b;
+}
+
+isLess(10, 15); 
+
+// function isLess(a, b){
+//    if (a === b) {
+//       return true;
+//   } else {
+//       return false;
+//   }
+// } 
+
+// We can eliminate the unnecessary lines of code, and just return the boolean value from the function using the less than operator. 
