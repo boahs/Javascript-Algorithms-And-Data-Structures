3a4b33cf7a2d0dfcf244b858dc22f6ae7718951c
boahs <39522778+boahs@users.noreply.github.com>
2020-04-23 23:52:16 -0400

    Create Build Javascript Objects.js

---
+++ Solutions/Build Javascript Objects.js
@@ -0,0 +1,8 @@
+var myDog = {
+    "name": "Doge",
+    "legs": 5,
+    "tails": 1,
+    "friends": ["Boahs", "Cats", "You"]
+    };
+// objects while similar to arrays are a little different due to the fact that you can use indexing to access, and modify their data. These pieces of data are called properties.
+    
