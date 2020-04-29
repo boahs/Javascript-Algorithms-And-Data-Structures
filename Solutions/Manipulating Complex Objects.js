c771322e0c02b5f07011b4de8914da48646aefba
boahs <39522778+boahs@users.noreply.github.com>
2020-04-29 11:56:30 -0400

    Create Manipulating Complex Objects.js

---
+++ Solutions/Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+    "artist": "Moonman",
+    "title": "Boahs learning nested objects",
+    "release_year": 2020,
+    "formats": [
+      ".mp4",
+      ".flv",
+      ".btt"
+    ]
+    }
+  ];
+  
