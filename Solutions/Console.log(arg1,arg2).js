3ec423012a7cc0a518ccd0b9b69276d5ee633c16
boahs <39522778+boahs@users.noreply.github.com>
2020-05-27 17:58:40 -0400

    Create Console.log(arg1,arg2).js

---
+++ Solutions/Console.log(arg1,arg2).js
@@ -0,0 +1,3 @@
+
+
+console.log(`%c Using the string substitution pattern C as your first character in a String, then passing in some CSS as your second arguement will result in this! The 🎂 is a lie. `, 'color: #C12957; font-weight: bold; background-color:#fffdd0;')
